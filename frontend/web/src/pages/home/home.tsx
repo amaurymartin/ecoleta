1af94161cbe0062bd67a9946e595361fc71257a3
Amaury Martin <amaury.conde@hotmail.com>
2021-06-11 22:40:34 -0300

    Fix home component

--- frontend/web/src/pages/home/home.tsx
+++ frontend/web/src/pages/home/home.tsx
@@ -8,16 +8,15 @@ import logo from '../../assets/logo.svg'
 
 const Home = () => {
   return (
-    <div id="page-home">
+    <div id="home">
       <div className="content">
         <header>
           <img src={logo} alt="Ecoleta" />
         </header>
 
         <main>
-          <h1>Your marketplace</h1>
-          <h1>for recycling</h1>
-          <p>We help people find collection points efficiently</p>
+          <h1>Your marketplace<br/>for recycling</h1>
+          <p>We help people to find collection points efficiently</p>
 
           <Link to="/collection-points/new">
             <span><FiLogIn /></span>
